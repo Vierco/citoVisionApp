15b955abc7ba423b41190bd3d6fc8ac0ad8b20be
Sergio Alvarez - Vierco <sergioalvarezb@gmail.com>
2026-07-23 18:14:28 +0200

    Update UI general devices (#15)
    * context: login screen size adjusted
    
    * context: Settings anda analysis screen size adjusted
    
    * context: Settings and analysis screen size adjusted

--- shared/src/commonMain/kotlin/dev/lovelace/citovision/presentation/screens/SettingsScreen.kt
+++ shared/src/commonMain/kotlin/dev/lovelace/citovision/presentation/screens/SettingsScreen.kt
@@ -414,7 +414,7 @@ fun SettingsScreen(
                 modifier =
                     Modifier
                         .fillMaxWidth()
-                        .padding(top = 8.dp),
+                        .padding(top = 32.dp),
             ) {
                 IconButton(
                     onClick = { onEvent(SettingsUiEvent.NavigateBack) },
